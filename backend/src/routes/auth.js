0673458f576a9131c18424e40ea9c9d984220df5
KartikShingde <kartikshingde12@gmail.com>
2025-11-07 11:50:46 +0530

    added sameSite:none

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -29,7 +29,7 @@ authRouter.post("/signup", async (req, res) => {
     res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
         httpOnly: true,
-        
+        sameSite:"none",
         path: "/",
       });
 
@@ -60,7 +60,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
         httpOnly: true,
-        
+        sameSite:"none",
         path: "/",
       });
 
