5ec467b0e8c18e61e51b3c09bc2f96a140559f81
KartikShingde <kartikshingde12@gmail.com>
2025-11-07 12:27:35 +0530

    disabled httpOnly

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -28,7 +28,6 @@ authRouter.post("/signup", async (req, res) => {
 
     res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
-        httpOnly: true,
         sameSite:"none",
         path: "/",
       });
@@ -59,7 +58,6 @@ authRouter.post("/login", async (req, res) => {
       // Add the token to cookie and send the response back to the User
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
-        httpOnly: true,
         sameSite:"none",
         path: "/",
       });
